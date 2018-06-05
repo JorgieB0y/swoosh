fb1dfc9c44059abe918923400c8c113cf7becb5f
Jorge Luis Velasco <jlvelasco1994@gmail.com>
2018-06-05 13:53:30 -0500

    Added UIViewController files to all three of the Storyboard Views, separated everything into proper folders/groups

---
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,19 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Jorge on 6/5/18.
+//  Copyright © 2018 Jorge Velasco Sanvodal. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Swoosh/Swoosh/Controller/SkillVC.swift
@@ -0,0 +1,19 @@
+//
+//  SkillVC.swift
+//  Swoosh
+//
+//  Created by Jorge on 6/5/18.
+//  Copyright © 2018 Jorge Velasco Sanvodal. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
